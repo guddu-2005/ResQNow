f259609f179aa3f7a1a29c4cda214df387a52417
smrutiranjan behera <bsmrutiranjan260@gmail.com>
2025-09-05 10:33:47 +0000

    FirebaseError: Failed to get document because the client is offline.

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,7 +1,7 @@
 
 import {initializeApp, getApps, getApp} from 'firebase/app';
 import {getAuth} from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore, enableIndexedDbPersistence } from 'firebase/firestore';
 
 const firebaseConfig = {
   projectId: 'rescueai-h6774',
@@ -17,5 +17,22 @@ const firebaseConfig = {
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+
+// Enable offline persistence
+if (typeof window !== 'undefined') {
+  try {
+    enableIndexedDbPersistence(db);
+  } catch (error: any) {
+    if (error.code == 'failed-precondition') {
+      // Multiple tabs open, persistence can only be enabled in one tab at a time.
+      // ...
+    } else if (error.code == 'unimplemented') {
+      // The current browser does not support all of the
+      // features required to enable persistence
+      // ...
+    }
+  }
+}
+
 
 export {app, auth, db};
